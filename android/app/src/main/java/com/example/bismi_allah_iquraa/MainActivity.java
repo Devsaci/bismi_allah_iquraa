3dd6988b4fac3f7fb638b72df435374d8217351d
saci zakaria <sacideveloppeur@gmail.com>
2022-04-24 17:34:00 +0200

    Initialis BISMI ALLAH IQRAE App

---
+++ android/app/src/main/java/com/example/bismi_allah_iquraa/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.bismi_allah_iquraa;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BISMI ALLAH IQRAE',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:  MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("BISMI ALLAH IQRAE"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              'BISMI ALLAH IQRAE',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
